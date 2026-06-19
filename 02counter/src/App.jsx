c2fcecae4840f4930a293296b774978dd4d00bdb
Hassan Usman <hu838982@gmail.com>
2026-06-19 17:21:05 +0500

    Updated reactcode

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -16,12 +16,20 @@ let  [counter, setCounter] = useState(1)
 // let counter = 1
 
 const AddValue = () =>{
+  if(counter >= 20){
+    alert("counter value should not be greater than 20")
+    return
+  }
   counter = counter + 1
   setCounter(counter)
   console.log("clicked", counter)
 }
 
 const RemoveValue = () =>{
+  if(counter <= 0){
+    alert("counter value should not be less than 0")
+    return
+  }
   counter = counter - 1
   setCounter(counter)
   console.log("clicked", counter)
