b72c9117c75447e7539f727ddb3cc5d2a2161954
Hassan Usman <hu838982@gmail.com>
2026-06-25 09:51:03 +0500

    added upper cas , lower case , numbers and symbols functionality in the Password Generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,15 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [includeUppercase, setIncludeUppercase] = useState(false)
+  const [includeLowercase, setIncludeLowercase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+  // password generation logic will be implemented here
+  const  [password, setPassword] = useState("")
+
+  //password gnerator function
+  const genratePassword = (()=>{
+    let pass = ""
+    let string = ""
+    let includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let includeLowercase = "abcdefghijklmnopqrstuvwxyz"
+    let includeNumbers = "0123456789"
+    let includeSymbols = "!@#$%^&*()-+=_<>?/"
+
+
+    if(includeUppercase){
+      pass += includeUppercase
+    }
+    if(includeLowercase){
+      pass += includeLowercase
+    }
+    if(includeNumbers){
+      pass += includeNumbers
+    }
+    if(includeSymbols){
+      pass += includeSymbols
+    }
+    
+
+    // Loop
+    for(let i = 1; i < Array.length; i++){
+      let character = Math.floor(Math.random() * pass.length +1) 
+      pass = str.charAt(character)
+
+      setPassword(pass)
+    }
+  } ,[length, includeUppercase, includeLowercase, includeNumbers , includeSymbols , setPassword])
 
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+    <div className="w-full  max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500  bg-gray-700 ">
+      <div className="flex-shadow rounded-lg  overflow-hidden mb-4">
+        <input type="text" 
+        value={password}
+          className="outline-none w-full py-1 px-3 "
+          placeholder="password"
+          readOnly
+          ></input>
+
+      </div>
+    </div>
     </>
   )
 }
