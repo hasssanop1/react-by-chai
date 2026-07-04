1496937e054adb402628b007753fe938ed4b86f3
Hassan Usman <hu838982@gmail.com>
2026-07-04 15:35:10 +0500

    made some changes

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -2,7 +2,15 @@ import {useState, useEffect} from 'react';
 
 function useCurrencyInfo(currency) {
     useEffect(() => {
-        fetch()
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => res.json())
+        
+
+
+
+
+
     } , [])
 
 }
